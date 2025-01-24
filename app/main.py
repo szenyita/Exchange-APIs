809040fd51d90d70b71d61470898a029711b46ff
szenyitamark <szenyita.mark@outlook.com>
2025-01-24 16:33:14 +0100

    App v13

--- app/main.py
+++ app/main.py
@@ -8,13 +8,12 @@ def main():
     exchangerates_success = exchangerates()
 
     while not currencylayer_success or not exchangerates_success:
+        time.sleep(5 * 60)
+
         if not currencylayer_success:
             currencylayer_success = currencylayer()
         if not exchangerates_success:
             exchangerates_success = exchangerates()
-
-        time.sleep(5 * 60)
-
 
 schedule.every().day.at("00:00").do(main)
 
