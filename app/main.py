448dfddaf0b6228afc75cfb59e5d9ba87406cd1d
szenyitamark <szenyita.mark@outlook.com>
2025-01-24 13:45:04 +0100

    App v9

--- app/main.py
+++ app/main.py
@@ -6,10 +6,6 @@ from data_functions.exchangerates import exchangerates
 def main():
     currencylayer_success = currencylayer()
     exchangerates_success = exchangerates()
-    return currencylayer_success, exchangerates_success
-
-def main_job():
-    currencylayer_success, exchangerates_success = main()
 
     while not currencylayer_success or not exchangerates_success:
         if not currencylayer_success:
@@ -18,9 +14,9 @@ def main_job():
             exchangerates_success = exchangerates()
 
         time.sleep(5 * 60)
-    
 
-schedule.every().day.at("00:00").do(main_job)
+
+schedule.every().day.at("00:00").do(main)
 
 while True:
     schedule.run_pending()
